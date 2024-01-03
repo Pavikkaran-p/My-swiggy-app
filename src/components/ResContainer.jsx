91344741c664ea390a26db6f90c07207d3769c13
pavikkaran67 <142660254+pavikkaran67@users.noreply.github.com>
2024-01-03 10:06:41 +0530

    commit

--- src/components/ResContainer.jsx
+++ src/components/ResContainer.jsx
@@ -14,7 +14,7 @@ const ResContainer = () => {
 
   const fetchdata = async () => {
     const res = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.0168445&lng=76.9558321&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0168445&lng=80.9558321&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json1 = await res.json();
     // console.log(json1);
